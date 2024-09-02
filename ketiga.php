72028cc7ec8a5895bb825ee33346f803e4a8fd16
Srynt <H1101231037@student.untan.ac.id>
2024-09-02 21:00:38 +0700

    Praktikum 4A - Membuat Class

---
+++ ketiga.php
@@ -0,0 +1,45 @@
+<?php
+class Lingkaran
+{
+    const PHI = 3.14;
+
+
+    public function luas($jari_jari) : float {
+        return self::PHI*$jari_jari*$jari_jari;
+    }
+
+
+    public function keliling($jari_jari) : float {
+        return 2*self::PHI*$jari_jari;
+    }
+}
+
+
+class Bola
+{
+    const PHI = 3.14;
+    public function volume($jari_jari) : float {
+        return (4/3)*self::PHI*pow($jari_jari,3);
+    }
+}
+
+
+class Tabung
+{
+    const PHI = 3.14;
+    public function volume($jari_jari,$tinggi) : float {
+        return self::PHI*pow($jari_jari,2)*$tinggi;
+    }
+}
+
+
+class Kerucut
+{
+    const PHI = 3.14;
+    public function volume($jari_jari,$tinggi) : float {
+        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    }
+}
+
+$nasi_tumpeng = new Kerucut();
+echo $nasi_tumpeng->Volume(4,10);
