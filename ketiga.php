ba227c8b94e4d8bd6225386145eafd86562c2b0b
Srynt <H1101231037@student.untan.ac.id>
2024-09-02 21:31:41 +0700

    Praktikum 4C - Membuat Constructor

--- ketiga.php
+++ ketiga.php
@@ -1,8 +1,15 @@
 <?php
+
+
 class Lingkaran
 {
     const PHI = 3.14;
     public $jari_jari;
+
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
 
 
     public function luas() : float {
@@ -16,10 +23,16 @@ class Lingkaran
 }
 
 
+
 class Bola
 {
     public $jari_jari;
     const PHI = 3.14;
+
+    public function __construct($jari_jari) {
+        $this->jari_jari = $jari_jari;
+    }
+
     public function volume() : float {
         return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
@@ -30,6 +43,12 @@ class Tabung
 {
     public $jari_jari;
     public $tinggi;
+
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi =$tinggi;
+    }
+    
     const PHI = 3.14;
     public function volume() : float {
         return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
@@ -42,26 +61,26 @@ class Kerucut
     public $jari_jari;
     public $tinggi;
     const PHI = 3.14;
+
+    public function __construct($jari_jari,$tinggi) {
+        $this->jari_jari = $jari_jari;
+        $this->tinggi =$tinggi;
+    }
+   
     public function volume() : float {
         return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
-$nasi_tumpeng = new Lingkaran();
-$nasi_tumpeng->jari_jari=4;
+$nasi_tumpeng = new Lingkaran(4);
 echo "Ini adalah luas seonggok nasi tumpeng {$nasi_tumpeng->luas()}\n";
 echo "Ini adalah keliling nazi tumpeng {$nasi_tumpeng ->Keliling()}\n";
 
-$bola_budi = new Bola();
-$bola_budi->jari_jari = 5;
+$bola_budi = new Bola(5);
 echo "Volume bola budi adalah {$bola_budi->volume()}\n";
 
-$tab= new Tabung();
-$tab->jari_jari = 6;
-$tab->tinggi = 7;
+$tab= new Tabung(6,7);
 echo "Volume tabung budi adaalh {$tab->volume()}\n";
 
-$ker= new Kerucut();
-$ker->jari_jari=8;
-$ker->tinggi=9;
+$ker= new Kerucut(8,9);
 echo "Volume kerucut nya adalah {$ker->volume()}\n";
